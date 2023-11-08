eb1eaa2b10a2ff9d01143925fee0329e4e2fef97
NotDaniMarch <mdaniil2@inbox.lv>
2023-11-08 21:48:24 +0200

    Initial commit

---
+++ dm22045_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control
+from dm22045_libadd import *
+from dm22045_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
